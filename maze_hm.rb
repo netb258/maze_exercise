37f55644e8f7f688fb627a5df0ab19edc0228ed2
netb258 <netb258@abv.bg>
2015-11-02 23:41:00 +0200

    Update maze_hm.rb

--- maze_hm.rb
+++ maze_hm.rb
@@ -22,28 +22,32 @@ def get_start_floor(the_maze)
   end 
 end
 
-#Returns true if we are at the exit of the maze (false otherwise).
-def found_exit(the_maze, floor, position) 
-  #If we have reached the most east, west, north or south side of the maze, then we are at the exit.
+#If we have reached the most east, west, north or south side of the maze, then we are at the exit.
+def found_exit?(the_maze, floor, position) 
   (floor == 0 or floor == the_maze.size - 1) or (position == 0 or position == the_maze[floor].size - 1)
 end
 
+#We can go right if our position does not exceed the boundaries of the maze AND we cannot step on an "x".
 def can_go_right?(the_maze, floor, position)
   (position + 1 < the_maze[floor].size) and (the_maze[floor][position + 1] == "0")
 end
 
+#We can go up if our floor does not pass floor 0 (it is the highest floor) AND we cannot step on an "x".
 def can_go_up?(the_maze, floor, position)
   (floor - 1 > -1) and (the_maze[floor - 1][position] == "0") 
 end
 
+#We can go left if our position does not pass position 0 (it is the leftmost position) AND we cannot step on an "x".
 def can_go_left?(the_maze, floor, position)
   (position - 1 > -1) and (the_maze[floor][position - 1] == "0") 
 end
 
+#We can go down if our floor does not exceed the boundaries of the maze AND we cannot step on an "x".
 def can_go_down?(the_maze, floor, position)
   (floor + 1 < the_maze.size) and (the_maze[floor + 1][position] == "0") 
 end
 
+#We go right by increasing our position by 1. Also, mark where we stepped with "m" (prevents going back and forth endlessly).
 def go_right(the_maze, floor, position, steps, paths)
   if can_go_right?(the_maze, floor, position)
     the_maze = the_maze.set(floor, the_maze[floor].set(position, "m"))
@@ -53,6 +57,8 @@ def go_right(the_maze, floor, position, steps, paths)
   end
 end
 
+#We go up by decreasing our floor by 1 (the highest floor is 0).
+#Also, mark where we stepped with "m" (prevents going back and forth endlessly).
 def go_up(the_maze, floor, position, steps, paths)
   if can_go_up?(the_maze, floor, position)
     the_maze = the_maze.set(floor, the_maze[floor].set(position, "m"))
@@ -62,6 +68,7 @@ def go_up(the_maze, floor, position, steps, paths)
   end
 end
 
+#We go left by decreasing our position by 1. Also, mark where we stepped with "m" (prevents going back and forth endlessly).
 def go_left(the_maze, floor, position, steps, paths)
   if can_go_left?(the_maze, floor, position)
     the_maze = the_maze.set(floor, the_maze[floor].set(position, "m"))
@@ -71,6 +78,7 @@ def go_left(the_maze, floor, position, steps, paths)
   end
 end
 
+#We go down by increasing our floor by 1. Also, mark where we stepped with "m" (prevents going back and forth endlessly).
 def go_down(the_maze, floor, position, steps, paths)
   if can_go_down?(the_maze, floor, position)
     the_maze = the_maze.set(floor, the_maze[floor].set(position, "m"))
@@ -82,9 +90,9 @@ end
 
 #Traverses the maze recursively and returns all possible paths as arrays of steps.
 #The steps are saved as arrays, each having 2 elements: the first is the current row, and second is the current column.
-#Note that the last two arguments (steps and paths) should be left at default when the user calls the function.
+#Note that the last two arguments (steps and paths) should be left at default when the user calls walk_maze.
 def walk_maze(the_maze, floor, position, steps=Hamster.vector, paths=Hamster.vector) 
-  if found_exit(the_maze, floor, position)
+  if found_exit?(the_maze, floor, position)
     #When we have found an exit, we add the steps array in paths.
     return paths.unshift(steps.push([floor, position]))
   end
